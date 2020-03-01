cc9fe8ac2d3ee70338add9bf91333a0814bea6ae
swc0620 <pinetreewave@naver.com>
2020-03-01 23:48:30 +0900

    Updates

---
+++ precache-manifest.115d0c1b24615b24f56e8c0559e959c2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "42bb1b8da4ec8940c5f57240ec99fc33",
+    "url": "/swc0620/movie_app_2019/index.html"
+  },
+  {
+    "revision": "6c371ecff60417609ef6",
+    "url": "/swc0620/movie_app_2019/static/css/main.18ce0043.chunk.css"
+  },
+  {
+    "revision": "475468186b9d129af88a",
+    "url": "/swc0620/movie_app_2019/static/js/2.137c9ea0.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/swc0620/movie_app_2019/static/js/2.137c9ea0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6c371ecff60417609ef6",
+    "url": "/swc0620/movie_app_2019/static/js/main.ae443270.chunk.js"
+  },
+  {
+    "revision": "17cc9660e26abc8e7fa8",
+    "url": "/swc0620/movie_app_2019/static/js/runtime-main.4d0ca58a.js"
+  }
+]);
